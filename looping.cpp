77d59c2740eb70c1b5f9b35edb9d5af2436db112
pipu-kun <SideActivity@proton.me>
2026-03-31 11:15:15 +0000

    Loop For

--- looping.cpp
+++ looping.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int main() {
     int x; 
+
+    cout << "Perulangan For " << endl;
+    for (int i = 0; i <= 4; i++) {
+        cout << "Teknologi Informasi UMY" << i << endl;
+        cout << endl;
+    }
+    
 }
